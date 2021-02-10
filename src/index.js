b5d5ad70849b90fa5e6f31ff5d737097fd77b830
vifi-on <ladiasman1992@gmail.com>
2021-02-10 15:38:31 +0300

    feat: add functions

--- src/index.js
+++ src/index.js
@@ -1,12 +1,27 @@
 
 exports.min = function min (array) {
-  return 0;
+  if (!array || array.length === 0) return 0 
+
+  const sortArr = array.sort((a, b) => a - b)
+  const minNum = sortArr.shift()
+
+  return minNum;
 }
 
 exports.max = function max (array) {
-  return 0;
+  if (!array || array.length === 0) return 0 
+
+  const sortArr = array.sort((a, b) => a - b)
+  const maxNum = sortArr.pop()
+
+  return maxNum;
 }
 
 exports.avg = function avg (array) {
-  return 0;
+  if (!array || array.length === 0) return 0 
+
+  const sumArr = array.reduce((a, b) => a + b, 0)
+  const avgNum = sumArr / array.length
+
+  return avgNum;
 }
